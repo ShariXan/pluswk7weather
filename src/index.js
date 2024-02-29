0317cbb7db3bcca4bbfae37520ca29285fa71331
ShariXan <158981154+ShariXan@users.noreply.github.com>
2024-02-29 22:09:38 +1030

    Added Days Time Wind
    Added Day of the week, time, wind speed to conditions of the weather site

--- src/index.js
+++ src/index.js
@@ -4,13 +4,39 @@ function updateWeather(response) {
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#condition");
   let humidityElement = document.querySelector("#humidity");
+  let knotsElement = document.querySelector("#knots");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
   console.log(response.data.condition.description);
 
   cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
-  humidityElement.innerHTML = response.data.temperature.humidity;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  knotsElement.innerHTML = `${response.data.wind.speed}km/h`;
   Math.round(temperature);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
