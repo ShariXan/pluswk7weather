37e512c3515d5e071ca23dcc6d88334202d54003
ShariXan <158981154+ShariXan@users.noreply.github.com>
2024-02-29 11:32:24 +1030

    Added current weather response
    Real time weather response js functions added
    
    Rounding of temperature in Celsius

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "5fe0ed6aba088342a8t4e2c63oeeb1fb";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
